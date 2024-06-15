371b483bff6f3a21f838253a050fcae5fed44797
LeonardoVinniciusLopesRibas <110777424+LeonardoVinniciusLopesRibas@users.noreply.github.com>
2024-06-15 15:11:29 -0300

    Primeiro Commit
    Criando

---
+++ src/main/java/educare/educarefrontend/EduCareFrontEndApplication.java
@@ -0,0 +1,13 @@
+package educare.educarefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCareFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduCareFrontEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/educare/educarefrontend/EduCareFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package educare.educarefrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCareFrontEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
